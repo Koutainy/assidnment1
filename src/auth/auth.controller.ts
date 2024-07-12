17b5c9c9b54d96ecbc09b03b219e5a56d12e5b85
koutainy <raneemkoutainy4@gmail.com>
2024-07-13 02:35:29 +0300

    *swagger api final test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
-
+import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiOperation({ summary: 'login user' })
+    @ApiResponse({ status: 200, description: 'Returns access token and user info' })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,23 +6,30 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { Role } from '../auth/role.enum';
-
+import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
+@ApiTags('users')
 @Controller('users')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':id')
+   @ApiOperation({ summary: 'Get user by id' })
+    @ApiParam({ name: 'id', type: String })
   findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update user by id' })
+  @ApiParam({ name: 'id', type: String })
   updateUser(@Param('id') id: string, @Body() updateUserDto: any): Promise<User> {
     return this.usersService.updateUser(id, updateUserDto);
   }
 
   @Get(':id/tasks')
+  @ApiOperation({ summary: 'find user task by ID' })
+  @ApiParam({ name: 'id', type: String })
   findUserTasks(@Param('id') id: string): Promise<Task[]> {
     return this.usersService.findUserTasks(id);
   }
